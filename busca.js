08fa321bbd8df0094c19e9b0f1103f56e1cf42fb
lucasrachid <lucasrachid@hotmail.com>
2021-07-07 19:56:01 -0300

    estrutura de dados - bolha e busca

---
+++ busca.js
@@ -0,0 +1,43 @@
+let valores = [5, 8, 10, 22, 38, 45, 60, 90, 100, 120];
+
+function busca(num) {
+  for (i = 0; i < 6; i++) {
+    if (num == valores[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+function buscaBin(num) {
+  let inicio, fim;
+  let meio;
+  let passos = 0;
+  inicio = 0;
+  fim = 9;
+  while (inicio <= fim) {
+    meio = parseInt((inicio + fim) / 2);
+    passos += 1;
+    if (num == valores[meio]) {
+      console.log("Achei em passos " + passos + " passos");
+      return meio;
+    } else {
+      if (num > valores[meio]) {
+        inicio = meio + 1;
+      } else {
+        fim = meio - 1;
+      }
+    }
+  }
+  console.log("Nao achei em " + passos + " passos");
+  return -1;
+}
+
+// Usando a nossa ferramenta de busca
+
+// console.log(busca(10));
+// console.log(busca(50));
+
+console.log(buscaBin(38));
+console.log(buscaBin(50));
+console.log(buscaBin(60));
